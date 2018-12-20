90fa16921734f70c833de8c8f1d19207990ad975
Nate C-K <nculwell@gmail.com>
2018-12-20 12:11:04 -0600

    Hide success messages.

--- server/recreatedb.sh
+++ server/recreatedb.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-su postgres -c "psql -f dropandcreatedb.sql"
-su nate -c "psql -d taskmaster -f dbsetup.sql"
+# Sending stdout to /dev/null gets rid of success messages,
+# leaving error (and notice) messages to get displayed.
+echo Drop and recreate database.
+su postgres -c "psql -f dropandcreatedb.sql" || exit 1
+echo Create schema.
+su nate -c "psql -d taskmaster -f dbsetup.sql" >/dev/null
 
